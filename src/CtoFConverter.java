a1d7b18718cdac575bd6231c3b0a50aaea0363d3
captain-dj <whardig10@gmail.com>
2023-10-01 18:24:22 -0400

    V1.2

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class CtoFConverter {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // user enters price
+        System.out.println("Enter F to convert to C: ");
+        double celsius = scanner.nextDouble();
+
+         double CtoF = celsius * 9/5 + 32;
+
+
+        System.out.println("your convershion is: " + CtoF);
+
+
+        scanner.close();
     }
 }
